2bc91c005e7c5ff42e19f4329b9d2754e25e5aa0
erkelost <1256029807@qq.com>
2022-01-02 16:01:55 +0800

    feat(mini): '封装轮播图组件 搜索组件'

--- pages/music/index.js
+++ pages/music/index.js
@@ -1,66 +1,41 @@
 // pages/home-music/index.js
+import {
+  getPhoneBanner
+} from '../../servise/api_video.js'
+import querySelector from '../../utils/querySelector'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    banner: [],
+    bannerHeight: 100
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getPageData()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleSearch() {
+    wx.navigateTo({
+      url: '/pages/search/index'
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  getPageData() {
+    getPhoneBanner().then(res => {
+      this.setData({
+        banner: res.banners
+      })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handelLoadImage() {
+    querySelector('.swiper-image').then(res => {
+      this.setData({
+        bannerHeight: res[0].height
+      })
+    })
   }
 })
